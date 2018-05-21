f40bcd936bbb58a3e2fbf5b03660d3b39675067c
s344951241 <344951241@qq.com>
2018-05-21 15:23:49 +0800

    1

--- Assets/Scripts/lua/ClassForLua.cs
+++ Assets/Scripts/lua/ClassForLua.cs
@@ -20,5 +20,6 @@ public class ClassForLua  {
     {
         callback.Call("c#");
     }
-    
+
+   
 }
---
+++ Assets/Scripts/tools/HotfixTool.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+
+public class HotfixTool{
+
+    [Hotfix]
+    public static void ShowString()
+    {
+        Debug.LogError("this is c# string");
+    }
+}
