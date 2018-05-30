c6517e54a1e0e7556cf65675dc7f7d33b37ea562
s344951241 <344951241@qq.com>
2018-05-30 20:20:10 +0800

    timeLine

---
+++ Assets/Scripts/timeLine/clip/ExampleClip.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ExampleClip : PlayableAsset
+{
+    [SerializeField]
+    private int m_ExampleId;
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<Behaviour>.Create(graph);
+        Behaviour behaviour = playable.GetBehaviour();
+        //设置behavior属性
+        behaviour.exampleId = m_ExampleId;
+        return playable;
+    }
+
+    private sealed class Behaviour : PlayableBehaviour
+    {
+        public int exampleId;
+        private bool tickFlag = false;
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            base.ProcessFrame(playable, info, playerData);
+            if (tickFlag)
+                return;
+            tickFlag = true;
+            Debug.Log("TimeLine Example 开始执行"+exampleId);
+            
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            base.OnBehaviourPause(playable, info);
+            Debug.Log("TimeLine Example 结束"+exampleId);
+        }
+    }
+}
---
+++ Assets/Scripts/timeLine/track/ExampleTrack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[Serializable]
+[TrackClipType(typeof(ExampleClip))]
+[TrackBindingType(typeof(GameObject))]
+public class ExampleTrack : TrackAsset {
+
+    public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+    {
+        return base.CreateTrackMixer(graph, go, inputCount);
+    }
+}
