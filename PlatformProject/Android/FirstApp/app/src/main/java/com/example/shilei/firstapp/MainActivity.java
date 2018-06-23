c7f6efe18bc4770e76e4c2a4599d792edf63eb04
s344951241 <344951241@qq.com>
2018-06-23 08:39:15 +0800

    android

--- PlatformProject/Android/FirstApp/app/src/main/java/com/example/shilei/firstapp/MainActivity.java
+++ PlatformProject/Android/FirstApp/app/src/main/java/com/example/shilei/firstapp/MainActivity.java
@@ -1,14 +1,13 @@
-package com.sl.game;
+package com.example.shilei.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.unity3d.player.UnityPlayerActivity;
-
-public class MainActivity extends UnityPlayerActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
